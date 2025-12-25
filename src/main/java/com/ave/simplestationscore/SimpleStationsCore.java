27ff10bab5e6cb6a35f1b0f23028d9ceadd57580
Andrejev Vladimir <andrejevve@gmail.com>
2025-12-25 14:20:27 +0200

    Create core

---
+++ src/main/java/com/ave/simplestationscore/SimpleStationsCore.java
@@ -0,0 +1,14 @@
+package com.ave.simplestationscore;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(SimpleStationsCore.MODID)
+public class SimpleStationsCore {
+        public static final String MODID = "simplestationscore";
+
+        public SimpleStationsCore(IEventBus modEventBus, ModContainer modContainer) {
+        }
+}
